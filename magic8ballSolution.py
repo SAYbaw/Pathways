5909781c291517e43182452b516b57b71c3c266a
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2024-10-20 14:53:27 -0400

    Create magic8ballSolution.py

---
+++ magic8ballSolution.py
@@ -0,0 +1,111 @@
+""" Magic 8 Ball
+    Programmer:[Enter your Name Here]
+    Partner: [If you are working with a partner and turning in the same code,  write the name here]
+    Shoutouts: [If you had some help from a classmate, give them some credit here] """
+
+""" Below import the random library module """
+
+"""[Your brilliant code here]"""
+import random
+
+""" Below initialize a string variable named playAgain to 'y' using the assignment operator = """
+
+"""[Your brilliant code here]"""
+playAgain = "y"
+
+""" Below initialize a variable named userName using the input() function.
+    For the prompt, type this string as the function's argument
+    'Welcome to the Magic 8 Ball.  Type your name and press enter:' """
+
+"""[Your brilliant code here]"""
+userName = input("Welcome to the Magic 8 Ball.  Type your name and press enter:")
+
+""" Use a print() function below to say 'Hello' and add the userName variable  as the second
+argument followed by a , """
+
+"""[Your brilliant code here]"""
+print("Hello", userName)
+
+""" Use another print() function to tell the user what you will do.
+You can be creative here or type this below:
+'Let me tell you your fortune', userName, '...', 'IF YOU DARE!' """
+
+"""[Your brilliant code here]"""
+print("Let me tell you your fortune... IF YOU DARE!")
+
+""" Here you will create a while loop header using the playAgain variable
+    this loop will continue while playAgain is equal to (==) 'y' """
+
+"""[Your brilliant code here]"""
+while playAgain == "y":
+
+    """ This is the loop body. Make sure you indent here
+    Below you will use an input() function to tell the user
+    to ask a question out loud. This will allow the program to
+    to pause while they ask their question. They will NOT
+    type their question, only press return. Think of this as a
+    pause, not a prompt for user data. the input function's prompt
+    argument should be 'Ask me a question out loud and press return' """
+
+    """[Your brilliant code here]"""
+    
+    input("Ask me a question out loud and press return")
+
+    """ Below, you will create a Python list (array) of all the possible
+    answers as strings. Call your list answers and initialize the list
+    using the assignment operator = and the list's content in a set of square 
+    brackets [] separated by commas ,
+    Here are all of the answers:
+    'It is certain', 'It is decidedly so', 'Without a doubt', 
+    'Yes – definitely', 'You may rely on it', 'As I see it, yes', 
+    'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
+    'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 
+    'Cannot predict now', 'Concentrate and ask again', 'Don't count on it',
+    'My reply is no', 'My sources say no', 'Outlook not so good',
+    'Very doubtful' """
+
+    """[Your brilliant code here]"""
+    
+    answers = ["It is certain", "It is decidedly so", "Without a doubt",\
+    "Yes – definitely", "You may rely on it", "As I see it, yes", "Most likely",\
+    "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again",\
+    "Ask again later", "Better not tell you now", "Cannot predict now",\
+    "Concentrate and ask again", "Don't count on it", "My reply is no",\
+    "My sources say no", "Outlook not so good", "Very doubtful"]
+
+    """ Use a print() function to give the user a randomly selected answer 
+    from the answers[] list. The first argument is answers[] and the second 
+    argument will be userName separated by a comma , and between the square 
+    brackets [] of answers[] will be a random integer that will be an index 
+    number from the answers[] list. Make sure to use random.randint() function 
+    here and the first argument will be the lowest index number 0 and the 
+    highest index number 19 """
+
+    """[Your brilliant code here]"""
+
+    print(answers[random.randint(0,19)], userName)
+
+    """ Below is an example of a print format function. It is another way to add 
+    variables or do special formatting in a print function. Notice the f after 
+    the left ( before the first quotation mark and you can use a place holder
+    by putting the variable name between curly braces {} """
+
+    print(f"Ask another question {userName}?")
+
+    """ Below you will assign a new value to the playAgain variable with an input() 
+    function. The argument to add as the prompt should be:
+    'press [y] for yes or any other key for no. Then press [return]:' """
+
+    """[Your brilliant code here]"""
+
+    playAgain = input("press [y] for yes or any other key for no. Then press [return]:")
+    
+""" Finally, add a closing message to the user by using a print() function. The first 
+argument should be something like 'Good luck' and the second argument will be the 
+userName variable. Feel free to be creative if you want. Remember this line will be 
+OUTSIDE of the loop so do not indent. Make sure it is lined up with the same indentation 
+as your while loop header. """
+
+"""[Your brilliant code here]"""
+
+print("Good Luck", userName)
